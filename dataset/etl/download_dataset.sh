185a8351e70e056713728c4a6cd6705ca038f883
Stefanos Baziotis <sdi1600105@di.uoa.gr>
2025-09-30 00:26:23 -0500

    Only download etl dataset if the tar.gz does not exist

--- dataset/etl/download_dataset.sh
+++ dataset/etl/download_dataset.sh
@@ -1,9 +1,11 @@
-
 #!/bin/bash
 
-wget https://zenodo.org/records/14260428/files/udfbench-dataset.tar.gz
+# Check if the tar.gz file already exists
+if [ ! -f "udfbench-dataset.tar.gz" ]; then
+    echo "Downloading udfbench-dataset.tar.gz..."
+    wget https://zenodo.org/records/14260428/files/udfbench-dataset.tar.gz
+else
+    echo "udfbench-dataset.tar.gz already exists, skipping download."
+fi
 
 tar -xvzf udfbench-dataset.tar.gz
-
-
-
