5ff13cfcf71bf1546507b72345f84d0e383c95f1
Stefanos Baziotis <sb54@illinois.edu>
2025-10-01 02:14:19 -0500

    Download from Google Drive

--- dataset/etl/download_dataset.sh
+++ dataset/etl/download_dataset.sh
@@ -3,7 +3,7 @@
 # Check if the tar.gz file already exists
 if [ ! -f "udfbench-dataset.tar.gz" ]; then
     echo "Downloading udfbench-dataset.tar.gz..."
-    wget https://zenodo.org/records/14260428/files/udfbench-dataset.tar.gz
+    gdown 18LVPSQmBqrQc8xpIGxZUWiFOyEkfghui
 else
     echo "udfbench-dataset.tar.gz already exists, skipping download."
 fi
