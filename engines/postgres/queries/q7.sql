32ec9f0ad3892312f0b8e02d1510bff0c2a9e0de
Stefanos Baziotis <sb54@illinois.edu>
2025-10-02 01:45:02 -0500

    [Postgres] Fix wrong absolute path

--- engines/postgres/queries/q7.sql
+++ engines/postgres/queries/q7.sql
@@ -1,4 +1,4 @@
 explain (analyse,buffers)
  select aggregate_avg(jsoncount(citations)),aggregate_avg(jsoncount(authors)) 
- from file('/app/dataset/files/small/pubmed_q7.txt','json') f(id text, citations text,authors text) ;
+ from file('pubmed_q7.txt','json') f(id text, citations text,authors text) ;
  
